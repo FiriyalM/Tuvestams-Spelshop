6fa3fca4fc318f1e8c84e2cd6dcc4110092a0d24
Nader Alhamwi <38947594+naderalhamwi@users.noreply.github.com>
2020-12-09 17:57:12 +0100

    Order filer skapades
    skapade alla Order filer

---
+++ Backend/src/main/java/com/te4/order/OrderBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.te4.order;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.te4.backend.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Elev
+ */
+@Stateless
+public class OrderBean {
+}
